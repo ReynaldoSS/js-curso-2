38a9ce5148c668c48721c9e21bb9bdd4b762de51
Reynaldo Souza Santos <reynaldo.ss@gmail.com>
2024-03-16 12:44:48 -0300

    Contanto tentativas

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ function exibirTextoNaTela(tag, texto) {
   campo.innerHTML = texto;
 }
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 exibirTextoNaTela("h1", "Jogo do número secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
   let chute = document.querySelector("input").value;
   if (chute == numeroSecreto) {
+    let palavraTentativas = tentativas == 1 ? "tentativa" : "tentativas";
+    let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativas}`;
     exibirTextoNaTela("h1", "Acertou");
-    exibirTextoNaTela("p", "Você descobriu o número secreto");
+    exibirTextoNaTela("p", mensagemTentativas);
   } else {
     if (chute > numeroSecreto) {
       exibirTextoNaTela("p", "O número secreto é menor");
     } else {
       exibirTextoNaTela("p", "O número secreto é maior");
     }
+    tentativas++;
   }
 }
 
