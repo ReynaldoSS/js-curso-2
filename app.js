d3125087fbaa11bea9bd7bd41513151dbfbe941e
Reynaldo Souza Santos <reynaldo.ss@gmail.com>
2024-03-16 12:39:49 -0300

    Verificando o chute

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
-
+let numeroSecreto = gerarNumeroAleatorio();
 exibirTextoNaTela("h1", "Jogo do número secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
 function verificarChute() {
-  console.log("verificar chute");
+  let chute = document.querySelector("input").value;
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela("h1", "Acertou");
+    exibirTextoNaTela("p", "Você descobriu o número secreto");
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela("p", "O número secreto é menor");
+    } else {
+      exibirTextoNaTela("p", "O número secreto é maior");
+    }
+  }
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
 }
